736512648d64d88d56fb991701c4230c97b96014
birkan.kolcu <birkan.kolcu@ozu.edu.tr>
2016-12-13 15:24:57 -0500

    changed mongoModule_test

--- back-end/test/unit/mongoModule_test.js
+++ back-end/test/unit/mongoModule_test.js
@@ -18,6 +18,12 @@
       and inserting the document i sent. However, i was deleting entire collection elements right after
       the insert operation because, beforeEach was calling the clearCollection function.
   this.timeout(5000); //Override 2000ms default mocha timeout for current "before", "beforeEach", "it", etc.
+  Normally, i was supposed to go with TDD approach where i would be writing test cases step by step and
+    implementing in scynchornously however this time, i wrote (probably not) all test cases at once because
+    i was supposed to give those tests to my teammate so that he would be implementing the functions according
+    to tests. Anyways, there is no reason for not to contniue to development with actualy TDD approach.
+  While "before" function runs before any other "it" function, it does not gurantee to first run stuff that are
+    inside "describe" but outside "it".
 */
 var should      = require('should');
 var MongoCrud   = require('mongo-crud-layer');
@@ -35,15 +41,13 @@ function generateRandomInt (low, high) {
 function clearCollection(callback) {
   mongocrud.delete({}, testCollection, callback);
 }
-/*
-    populateRandomScoreObjects(5,function(err, isSucceed){ //Just for experimenting, i'll populate db with 5 objects.
-      if (!err) {
-        console.log("populated 5 score obj");
-      } else {
-        throw new Error(err);
-      }
-    });
-*/
+
+function getScoreCollectionAsArray(callback) {
+  mongocrud.readAll(testCollection, function(err, docs) {
+    callback(err, docs);
+  });
+}
+
 function populateRandomScoreObjects(objectCount,callback){//Here, the point is to call the callback when all funcs are completed.
   var randomInt = generateRandomInt (0, 99999);
   var completedCallCount = 0;
@@ -76,7 +80,6 @@ describe('mongoModule.js', function(){
     mongoModule.initializeModule(mongoUrl, testCollection);
     done();
   });
-
   beforeEach(function(done) {
       clearCollection(function(err, result){
         if(!err){
@@ -92,71 +95,77 @@ describe('mongoModule.js', function(){
     var score         = 500;
     var dateInMillis  = new Date().getTime();
     var clientId      = 123123213;
-    it("should insert score object", function(){
+    it("should insert score object", function(done){
       mongoModule.saveScore(userName, score, dateInMillis, clientId, function(err, result){
         mongocrud.read({ "clientId": clientId }, testCollection, function(err, doc) {
-            console.log(doc); // { _id: 55617c9226d7023b19edcdd1, name: "Athyrion" } 
-            should.not.exist(err);
+            console.log(JSON.stringify(doc)); // { _id: 55617c9226d7023b19edcdd1, name: "Athyrion" } 
+            //should.not.exist(err);
+            should.not.exist({"yo":"aha"});
+            done();
         });
       });
     });
-    it("should return err object with 'invalid argument' message if userName with empty string entered", function(){
+    it("should return err object with 'invalid argument' message if userName with empty string entered", function(done){
       mongoModule.saveScore("", score, dateInMillis, clientId, function(err, result){
             should.exist(err);
             err.message.should.be.equal("invalid argument");
+            done();
       });
     });
-    it("should return err object with 'invalid argument' message on any null argument", function(){ //Is calling saveScore four times for each null is OK or not?
+    it("should return err object with 'invalid argument' message on any null argument", function(done){ //Is calling saveScore four times for each null is OK or not?
       var clientId_      = clientId;
       mongoModule.saveScore(null, score, dateInMillis, clientId_, function(err, result){
+        should.exist(err);
+        err.message.should.be.equal("invalid argument");
+        mongoModule.saveScore(userName, null, dateInMillis, ++clientId_, function(err, result){
           should.exist(err);
           err.message.should.be.equal("invalid argument");
+          mongoModule.saveScore(userName, score, null, ++clientId_, function(err, result){
+            should.exist(err);
+            err.message.should.be.equal("invalid argument");
+            mongoModule.saveScore(userName, score, dateInMillis, null, function(err, result){
+              should.exist(err);
+              err.message.should.be.equal("invalid argument");
+              done();
+            });
+          });
+        });
       });
-      mongoModule.saveScore(userName, null, dateInMillis, ++clientId_, function(err, result){
-          should.exist(err);
-          err.message.should.be.equal("invalid argument");
       });
-      mongoModule.saveScore(userName, score, null, ++clientId_, function(err, result){
-          should.exist(err);
-          err.message.should.be.equal("invalid argument");
-      });
-      mongoModule.saveScore(userName, score, dateInMillis, null, function(err, result){
-          should.exist(err);
-          err.message.should.be.equal("invalid argument");
-      });
-    });
-    it("should return err object with 'invalid argument' message on any type mismatch", function(){
+    it("should return err object with 'invalid argument' message on any type mismatch", function(done){
       var clientId_      = clientId;
       mongoModule.saveScore(5, score, dateInMillis, clientId_, function(err, result){
+        should.exist(err);
+        err.message.should.be.equal("invalid argument");
+        mongoModule.saveScore(userName, "invalid_score", dateInMillis, ++clientId_, function(err, result){
           should.exist(err);
           err.message.should.be.equal("invalid argument");
-      });
-      mongoModule.saveScore(userName, "invalid_score", dateInMillis, ++clientId_, function(err, result){
-          should.exist(err);
-          err.message.should.be.equal("invalid argument");
-      });
-      mongoModule.saveScore(userName, score, "invalid_dateInMillis", ++clientId_, function(err, result){
-          should.exist(err);
-          err.message.should.be.equal("invalid argument");
-      });
-      mongoModule.saveScore(userName, score, dateInMillis, "invalid_clientId", function(err, result){
-          should.exist(err);
-          err.message.should.be.equal("invalid argument");
+          mongoModule.saveScore(userName, score, "invalid_dateInMillis", ++clientId_, function(err, result){
+            should.exist(err);
+            err.message.should.be.equal("invalid argument");
+            mongoModule.saveScore(userName, score, dateInMillis, "invalid_clientId", function(err, result){
+              should.exist(err);
+              err.message.should.be.equal("invalid argument");
+              done();
+            });
+          });
+        });
       });
     });
-    it("should return err object with 'invalid argument' on negative score, dateInMillis or clientId", function(){
+    it("should return err object with 'invalid argument' on negative score, dateInMillis or clientId", function(done){
       var clientId_ = clientId;
       mongoModule.saveScore(userName, -1, dateInMillis, clientId_, function(err, result){
+        should.exist(err);
+        err.message.should.be.equal("invalid argument");
+        mongoModule.saveScore(userName, score, -2, ++clientId_, function(err, result){
           should.exist(err);
           err.message.should.be.equal("invalid argument");
-      });
-      mongoModule.saveScore(userName, score, -2, ++clientId_, function(err, result){
-          should.exist(err);
-          err.message.should.be.equal("invalid argument");
-      });
-      mongoModule.saveScore(userName, score, dateInMillis, -5, function(err, result){
-          should.exist(err);
-          err.message.should.be.equal("invalid argument");
+          mongoModule.saveScore(userName, score, dateInMillis, -5, function(err, result){
+            should.exist(err);
+            err.message.should.be.equal("invalid argument");
+            done();
+          });
+        });
       });
     });
   });
@@ -167,19 +176,58 @@ describe('mongoModule.js', function(){
       "dateInMillis": new Date().getTime(),
       "clientId": 999
     }
-        it("should return an array with ten object if collection document count exceeds ten", function(){
-          //load 11 element
-        });
-        it("should return an array with x object if collection document count x <= 10", function(){
-          //load 5 element
-        });
-    //insertViaWrapper
-      //it should get top ten list
+    it("should return an array with ten object if collection document count exceeds ten", function(done){
+      //this.timeout(5000);
+      populateRandomScoreObjects(11,function(err, isSucceed){
+        if (!err) {
+          mongoModule.getTopTenList(function(err, result){
+            should.not.exist(err);
+            should.exist(result);
+            result.should.be.an.Array();
+            result.length.should.be.equal(11);
+            done();
+          });
+        } else {
+          throw new Error(err);
+        }
+      });
+    });
+    it("should return an array with x object if collection document count x <= 10", function(done){
+      populateRandomScoreObjects(3,function(err, isSucceed){
+        if (!err) {
+          mongoModule.getTopTenList(function(err, result){
+            should.not.exist(err);
+            should.exist(result);
+            result.should.be.an.Array();
+            result.length.should.be.equal(3);
+            done();
+          });
+        } else {
+          throw new Error(err);
+        }
+      });
+    });
   });
   describe('clearAllScores()', function(){
-    //insertViaWrapper
-      //it should clear all scores
-        //findViaWrapper
+    it("should return array with zero length", function(done){
+      populateRandomScoreObjects(3,function(err, isSucceed){
+        if (!err) {
+          mongoModule.clearAllScores(function(err){
+            should.not.exist(err);
+            getScoreCollectionAsArray(function(err, docs) {
+              if (!err) {
+                docs.length.should.be.equal(0);
+                done();
+              } else {
+                throw new Error(err);
+              }
+            });
+          });
+        } else {
+          throw new Error(err);
+        }
+      });
+    });
   });
   after(function(){
     //dropCollectionViaWrapper
